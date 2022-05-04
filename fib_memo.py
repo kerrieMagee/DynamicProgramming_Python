30f17abef4195cf0755a7669b9542b8050d7cd21
Kerrie Magee <kerriemagee4@gmail.com>
2022-05-04 13:09:02 +1000

    Add files via upload

---
+++ fib_memo.py
@@ -0,0 +1,17 @@
+def fib_memo(n, memo):
+    """Fibonacci with memoisation (remembering the solutions to previously computed sub-problems
+    Time Complxity: Is O(n) - call to fib_memo is constant as each problem computed no more than
+    once with n sub-problems
+    """
+
+    #base case
+    if n <= 1:
+        return n
+    if memo[n] is None:
+        memo[n] = fib_memo(n-1, memo) + fib_memo(n-2, memo)
+    return memo[n]
+
+def fib(n):
+    memo = [None] * (n+1)
+    return fib_memo(n, memo)
+
